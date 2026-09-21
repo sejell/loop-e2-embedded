6e3f4ab8a8dbb5d5f748658fdc457db7342460ef
Sejal <sejal.regmi@gmail.com>
2026-09-21 10:29:11 -0400

    Add Zephyr buzzer driver

--- src/main.c
+++ src/main.c
@@ -1,11 +1,28 @@
-//#include <zephyr/kernel.h>
 #include <stdio.h>
+#include <zephyr/kernel.h>
+
+#include "buzzer.h"
 
 int main(void)
 {
     printf("E2 Buzzer Project Started!\n");
 
+    if (buzzer_init() != 0) {
+        printf("Buzzer initialization failed!\n");
+        return 0;
+    }
+
+    printf("Buzzer initialized successfully.\n");
+
+    while (1) {
+        printf("Buzzer ON for 2 seconds\n");
+
+        buzzer_play(2000);
+
+        printf("Buzzer OFF for 2 seconds\n");
+
+        k_msleep(2000);
+    }
+
     return 0;
 }
-// Created by sejal on 9/20/2026.
-//
